f475c1fa3010b500852a1d751b49f6725fc7f898
Elis Ruiz <earr0119@hotmail.com>
2023-06-07 09:48:47 -0400

    first commit

---
+++ src/main/java/org/skillsgrowth/java_web_final_project/EmployeesAppApplication.java
@@ -0,0 +1,13 @@
+package org.skillsgrowth.java_web_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skillsgrowth/java_web_final_project/EmployeesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skillsgrowth.java_web_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
